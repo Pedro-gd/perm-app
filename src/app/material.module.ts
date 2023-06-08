6f4a1278c5089ee5682ebad04688990075496f05
Pedro-gd <80784527+Pedro-gd@users.noreply.github.com>
2023-06-08 13:02:54 -0600

    2 files modified

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {MatGridListModule} from '@angular/material/grid-list';
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 const myModules: any = [
   MatButtonModule,
   MatCardModule,
   MatInputModule,
   MatIconModule,
-  MatGridListModule
+  MatGridListModule,
 ];
 
 @NgModule({
-
-  imports: [... myModules],
-  exports: [... myModules]
+  imports: [...myModules],
+  exports: [...myModules],
 })
-export class MaterialModule { }
+export class MaterialModule {}
